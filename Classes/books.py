be116072c7c79a8ada35531e118a530c40f16a11
sofian.hussein@eduvaud.ch <sofian.hussein@eduvaud.ch>
2025-12-17 14:23:02 +0100

    ajout de la relation de book avec publisher

--- Classes/books.py
+++ Classes/books.py
@@ -3,10 +3,12 @@ Auteur : Rodrigo Silva Riço
 Date : 03.13.2025
 Projet : Création du fichier qui contiendra la classe Books
 """
-from sqlalchemy import Integer, String, Date
-from sqlalchemy.orm import Mapped, MappedColumn
+from sqlalchemy import Integer, String, Date, ForeignKey
+from sqlalchemy.orm import mapped_column, Mapped, relationship
+from publisher import Publisher
 
 from db.database import Base
+
 
 class Book(Base):
     __tablename__ = 'book'
@@ -16,13 +18,17 @@ class Book(Base):
     Il est toujours possible d'y accéder depuis l'extérieur de la classe (ex: instance._id).
     '''
 
-    #liste des attributs
-    _id: Mapped[int] = MappedColumn(Integer, primary_key=True)
-    _title: Mapped[str] = MappedColumn(String(200), nullable=False)
-    _nb_pages: Mapped[int] = MappedColumn(Integer, nullable=False)
-    _genre: Mapped[str] = MappedColumn(String(25), nullable=False)
-    _summary: Mapped[str] = MappedColumn(String(200), nullable=False)
-    _format: Mapped[str] = MappedColumn(String(15), nullable=False)
-    _release_date: Mapped[Date] = MappedColumn(Date, nullable=False)
-    _image: Mapped[str] = MappedColumn(String(200), nullable=False)
-    _status: Mapped[str] = MappedColumn(String(25), nullable=False)
+    # liste des attributs
+    _id: Mapped[int] = mapped_column(Integer, primary_key=True)
+    _title: Mapped[str] = mapped_column(String(200), nullable=False)
+    _nb_pages: Mapped[int] = mapped_column(Integer, nullable=False)
+    _genre: Mapped[str] = mapped_column(String(25), nullable=False)
+    _summary: Mapped[str] = mapped_column(String(200), nullable=False)
+    _format: Mapped[str] = mapped_column(String(15), nullable=False)
+    _release_date: Mapped[Date] = mapped_column(Date, nullable=False)
+    _image: Mapped[str] = mapped_column(String(200), nullable=False)
+    _status: Mapped[str] = mapped_column(String(25), nullable=False)
+
+    # création
+    publisher_id: Mapped[int] = mapped_column(ForeignKey("publishers.id"))
+    publisher: Mapped["Publisher"] = relationship("Publisher")
